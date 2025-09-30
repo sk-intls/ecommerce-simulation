3bedf72586497549c7abf215bf95dbf3982949c5
Sergey Karakash <serhii.karakash@smartly.io>
2025-09-30 21:14:06 +0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+async function main() {
+  console.log("Ecommerce app starting...");
+  console.log("=========================\n");
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
